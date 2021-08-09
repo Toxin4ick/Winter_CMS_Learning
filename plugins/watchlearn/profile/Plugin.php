7d106e48ad4ed919b299c3d843a7a5f32c0c6cb8
Toxin <toks0@mail.ru>
2021-08-09 12:54:30 +1000

    Extending User Plugin

--- plugins/watchlearn/profile/Plugin.php
+++ plugins/watchlearn/profile/Plugin.php
@@ -2,6 +2,7 @@
 
 use System\Classes\PluginBase;
 use Rainlab\User\Controllers\Users as UsersController;
+use Rainlab\User\Models\User as UserModel;
 
 class Plugin extends PluginBase
 {
@@ -16,6 +17,14 @@ class Plugin extends PluginBase
 
     public function boot()
     {
+        UserModel::extend(function ($model){
+            $model->addFillable([
+                'facebook',
+                'bio'
+            ]);
+        });
+
+
         UsersController::extendFormFields(function($form, $model, $context){
             $form->addTabFields([
                 'facebook'=>[
