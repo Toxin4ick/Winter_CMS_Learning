ab2b8d2147fa60d32b38a14d0c2f98c680e2f85d
Toxin <toks0@mail.ru>
2021-08-13 18:32:40 +1000

    Seeding With Routes

---
+++ plugins/watchlearn/movies/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use Watchlearn\Movies\Models\Movie;
+use Watchlearn\Movies\Models\Actor;
+
+Route::get('seed-actors', function (){
+
+    $faker = Faker\Factory::create();
+    for ($i = 0; $i < 100; $i++) {
+        Actor::create([
+            'name' => $faker->firstName,
+            'lastName' => $faker->lastName,
+        ]);
+    }
+
+
+    return "Actors created";
+});
